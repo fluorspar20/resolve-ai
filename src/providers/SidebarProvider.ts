a5f339e51f52d747949f183769b1587f32cf6d15
Amogh Dixit <amoghdixit104@gmail.com>
2024-04-20 22:58:34 +0530

    identify conflicted parts

--- src/providers/SidebarProvider.ts
+++ src/providers/SidebarProvider.ts
@@ -1,8 +1,7 @@
 import * as vscode from "vscode";
 import { getNonce } from "../utils/getNonce";
-import simpleGit, {SimpleGit} from 'simple-git';
-import * as fs from 'fs';
-import * as path from 'path';
+import * as cp from 'child_process';
+import { readFile } from "fs";
 
 export class SidebarProvider implements vscode.WebviewViewProvider {
   _view?: vscode.WebviewView;
@@ -24,81 +23,83 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
 
     webviewView.webview.onDidReceiveMessage(async (data) => {
       switch (data.type) {
-        case "onInfo": {
-          if (!data.value) {
-            return;
-          }
-          vscode.window.showInformationMessage(data.value);
-          break;
-        }
-        case "onError": {
-          if (!data.value) {
-            return;
-          }
-          vscode.window.showErrorMessage(data.value);
-          break;
-        }
         case "fetchCode": {
-          console.log('message', data.options.sourceBranch, data.options.remoteUrl);
-          const {sourceBranch, remoteUrl} = data.options;
-          //   // repo urls are of form - https://github.com/<username>/<repo_name>.git
-          // const repoName = remoteUrl.split('/').slice(-1)[0].replace('.git','');
-          // const tempDir = 'C:/Amogh/Projects/test';
-          // const git: SimpleGit = simpleGit();
+          const {sourceBranch, remoteOrigin} = data.options;
+          try {
+            const workspaceFolder = vscode.workspace.workspaceFolders?.[0]; // Assuming there is only one workspace folder open
 
-          // try {
-            
-          //   await git.cwd(tempDir).clone(remoteUrl);
-          //     // Checkout the desired branch
-          //   await git.cwd(`${tempDir}/${repoName}`).checkout(sourceBranch);
-          //   console.log('Code for branch', sourceBranch, 'fetched successfully');
-          // } catch(err){
-          //   console.error('Error fetching code: ', err);
-          // }
-          
-          const workspaceFolder = vscode.workspace.workspaceFolders?.[0]; // Assuming there is only one workspace folder open
+            if(workspaceFolder) {
 
-          // if (workspaceFolder) {
-          //     // Get the Git extension
-          //     const gitExtension = vscode.extensions.getExtension('vscode.git');
-          //     if (gitExtension) {
-          //         const gitAPI = gitExtension.exports.getAPI(1);
+              
+              // const terminal = vscode.window.createTerminal();
+              // terminal.sendText(`cd "${workspaceFolder.uri.fsPath}"`);
+              // terminal.sendText(`git fetch ${remoteOrigin} ${sourceBranch}`);
+              
+              // @todo - fetch this branch using the remote url
+              await this.executeGitCommand(`cd "${workspaceFolder.uri.fsPath}"`, workspaceFolder);
 
-          //         // Get the repository for the active workspace folder
-          //         const repository = gitAPI.repositories.find((repo: { rootUri: { fsPath: string; }; }) => repo.rootUri?.fsPath === workspaceFolder.uri.fsPath);
-          //         if (repository) {
-          //             // Get the remote URLs associated with the repository
-          //             const remoteUrls = repository.state.remotes.map((remote: { fetchUrl: any; }) => remote.fetchUrl);
-          //             console.log(remoteUrls);
-          //         }
-          //     }
-          // }
+              await this.executeGitCommand(`git fetch ${remoteOrigin} ${sourceBranch}`, workspaceFolder);
 
-          if(workspaceFolder) {
-            const terminal = vscode.window.createTerminal();
-            terminal.sendText(`cd "${workspaceFolder.uri.fsPath}"`);
-            terminal.sendText(`git fetch origin ${sourceBranch}`);
+              console.log(`Branch '${sourceBranch}' fetched successfully`);
 
-            console.log(`Branch '${sourceBranch}' fetched successfully`);
+              // terminal.sendText(`git merge --no-commit ${sourceBranch}`);
+              await this.executeGitCommand(`git merge --no-commit ${sourceBranch}`, workspaceFolder);
+              
+              // @todo - identify conflicted files
+              const output = await this.executeGitCommand(`git status --porcelain`, workspaceFolder);
+              console.log(output);
+              const conflictedFiles = output.split('\n')
+                  .filter(file => file.startsWith('UU')).map(file => file.split(' ')[1]); // 'UU' indicates merge conflict
+              console.log('conflicted files: ', conflictedFiles);
 
-            terminal.sendText(`git merge --no-commit ${sourceBranch}`);
-            // const conflictMarkers: string[] = [];
-            // terminal.onDidWriteData(data => {
-            //     // Parse the terminal output to find conflict markers
-            //     const matches = data.match(/<{7}([\s\S]*?)={7}([\s\S]*?)>{7}/g);
-            //     if (matches) {
-            //         conflictMarkers.push(...matches);
-            //     }
-            // });
+              // @todo - extract conflicted parts in those files
+              let conflictedParts = [];
+              for(const file of conflictedFiles) {
+                const filePath = `${workspaceFolder.uri.fsPath}\\${file}`;
+                // const output = await this.executeGitCommand(`git diff ${filePath}`, workspaceFolder);
+                const output = await this.readFile(filePath);
+                console.log(JSON.stringify(output));
+                const matches = [...output.matchAll(/<<<<<<< HEAD[\s\S]*?=======[\s\S]*?>>>>>>> [a-z0-9]+/g)];
+                conflictedParts.push({filePath, conflicts: matches.map(match => match[0])});
+              }
+
+              console.log(conflictedParts);
+
+            }
+          } catch (err) {
+            vscode.window.showErrorMessage("Some error occured.");
+            console.log(err);
+            break;
           }
-
-
-                
-
         }
       }
     });
   }
+
+  private executeGitCommand = async (command: string, workspaceFolder: vscode.WorkspaceFolder) => {
+    return new Promise<string>((resolve, reject) => {
+        cp.exec(command, {cwd: workspaceFolder.uri.fsPath}, (error, stdout, stderr) => {
+            if (error && error.code !== 1) {
+                console.log(error.message, error.code);
+                reject(new Error(`Command execution failed: ${stderr}`));
+            } else {
+                resolve(stdout);
+            }
+        });
+    });
+  };
+
+  private readFile = async (filePath: string) => {
+    return new Promise<string>((resolve, reject) => {
+      readFile(filePath, 'utf8', (err, data) => {
+          if (err) {
+              reject(err);
+          } else {
+              resolve(data);
+          }
+      });
+  });
+  };
 
   public revive(panel: vscode.WebviewView) {
     this._view = panel;
